13286426d231c146b167b290946e55c982b1b6d3
girllovevanilla <a10561344@gmail.com>
2023-11-12 23:58:41 -0500

    Create DeskPager.java

---
+++ src/pojo/DeskPager.java
@@ -0,0 +1,14 @@
+package pojo;
+import pojo.Pager;
+
+public class DeskPager extends Pager {
+    private String serchDeskCode;
+
+    public String getSerchDeskCode() {
+        return serchDeskCode;
+    }
+
+    public void setSerchDeskCode(String serchDeskCode) {
+        this.serchDeskCode = serchDeskCode;
+    }
+}
