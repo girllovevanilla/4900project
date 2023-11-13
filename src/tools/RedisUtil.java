4f8ee691179f4505fdaa6f94c3320051a8f2171a
girllovevanilla <a10561344@gmail.com>
2023-11-13 00:04:45 -0500

    Create RedisUtil.java

---
+++ src/tools/RedisUtil.java
@@ -0,0 +1,81 @@
+package tools;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import pojo.Desk;
+import pojo.DeskInfo_detail;
+
+import com.alibaba.fastjson.JSON;
+import pojo.Dish;
+@Component("redisUtil")
+public class RedisUtil {
+	@Resource(name = "redisTemplate")
+	private RedisTemplate redisTemplate;
+	private Logger logger = Logger.getLogger(String.valueOf(RedisUtil.class));
+
+	public void add(String key, String value) {
+		if (redisTemplate == null) {
+			logger.warning("redisTemplate Instantiation failure");
+			return;
+		} else {
+			redisTemplate.opsForValue().set(key, value);
+			logger.info("Add Successfully------><"+key+","+value+">");
+		}
+	}
+
+	public void addObj(String key, Object obj) {
+		if (redisTemplate == null) {
+			logger.warning("redisTemplate Instantiation failure");
+			return;
+		} else {
+			String objJson = JSON.toJSONString(obj);
+			logger.info("objJson----->"+objJson);
+			redisTemplate.opsForValue().set(key, objJson);
+			logger.info("Add Successfully------><"+key+","+objJson+">");
+		}
+	}
+	
+	public void addList(String key,List list){
+		if (redisTemplate == null) {
+			logger.warning("redisTemplate Instantiation failure");
+			return;
+		} else {
+			String listJson = JSON.toJSONString(list);
+			logger.info("objJson----->"+listJson);
+			redisTemplate.opsForValue().set(key, listJson);
+			logger.info("Add Successfully------><"+key+","+listJson+">");
+		}		
+	}
+		
+    public String get(String key) {
+        String value = (String) redisTemplate.opsForValue().get(key);
+        return value;
+    }
+    public Object getObj(String key) {
+        Desk value =JSON.parseObject((String)redisTemplate.opsForValue().get(key), Desk.class);
+        return value;
+    }
+    
+    public List getList(String key) {
+		List value=null;
+        if("allDishes".equals(key)){
+			value=JSON.parseArray((String)redisTemplate.opsForValue().get(key), Dish.class);
+		}else{
+			 value =JSON.parseArray((String)redisTemplate.opsForValue().get(key), DeskInfo_detail.class);
+		}
+        return value;
+    }
+    
+    public void del(String key){
+    	 redisTemplate.delete(key);
+    	logger.info("Delete-----"+key+"------Success");
+    }
+	
+	
+}
